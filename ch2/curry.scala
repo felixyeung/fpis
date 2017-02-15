a884ea445e80e12ddc9b2029b2e25547f4eb3538
Felix Yeung <felix.j.yeung@gmail.com>
2017-02-14 22:07:05 -0800

    curry

---
+++ ch2/curry.scala
@@ -0,0 +1,10 @@
+object Curry {
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def main(args: Array[String]): Unit = {
+  }
+}
